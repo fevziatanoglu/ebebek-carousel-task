dfa1b9f7f4292b982001d304597e17d6582bf7d9
Fevzi Atanoğlu <95905332+fevziatanoglu@users.noreply.github.com>
2025-09-21 12:46:26 +0300

    Refactor carousel script for improved readability and structure

--- FEVZI_ATANOGLU.js
+++ FEVZI_ATANOGLU.js
@@ -1,32 +1,49 @@
 (() => {
 
-    const init = () => {
+  const init = () => {
 
-        if (location.hostname !== "www.e-bebek.com" || location.pathname !== "/") {
-            console.log("wrong page");
-            return;
-        }
-
-        buildHTML();
-        buildCSS();
-        setEvents();
+    if (location.hostname !== "www.e-bebek.com" || location.pathname !== "/") {
+      console.log("wrong page");
+      return;
     }
 
-    const buildHTML = () => {
-       
-    };
+    buildHTML();
+    buildCSS();
+    setEvents();
+  }
 
-
-
-    const buildCSS = () => {
-
+  const buildHTML = () => {
+    const section2A = document.querySelector('.Section2A');
+    if (!section2A) {
+      console.log("Section2A not found");
+      return;
     }
 
-    const setEvents = () => {
+    const carouselHTML = `
+      <div class="carousel">
+        <div class="container">
+          <h2 class="carousel-title">Sizin için Seçtiklerimiz/h2>
+          <div class="carousel-wrapper">
+            <button class="carousel-button left"><span class="toys-icon toys-icon-arrow-left"></span></button>
+            <button class="carousel-button right"><span class="toys-icon toys-icon-arrow-right"></span></button>
+          </div>
+        </div>
+      </div>
+    `;
 
-    }
+    section2A.insertAdjacentHTML('beforebegin', carouselHTML);
+  };
 
-    init();
+
+  const buildCSS = () => {
+
+  }
+
+  const setEvents = () => {
+
+  }
+
+  init();
 
 
 })();
