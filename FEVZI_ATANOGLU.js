27c8d742d7ce333f6ae922ea3a7bc29f7ae3f2df
Fevzi Atanoğlu <95905332+fevziatanoglu@users.noreply.github.com>
2025-09-21 12:23:23 +0300

    Initialize carousel script with basic structure and page validation

---
+++ FEVZI_ATANOGLU.js
@@ -0,0 +1,32 @@
+(() => {
+
+    const init = () => {
+
+        if (location.hostname !== "www.e-bebek.com" || location.pathname !== "/") {
+            console.log("wrong page");
+            return;
+        }
+
+        buildHTML();
+        buildCSS();
+        setEvents();
+    }
+
+    const buildHTML = () => {
+       
+    };
+
+
+
+    const buildCSS = () => {
+
+    }
+
+    const setEvents = () => {
+
+    }
+
+    init();
+
+
+})();
